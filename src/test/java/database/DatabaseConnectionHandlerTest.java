751353b76ac227f126eeb6c0cec369878a836d58
Thomas Burke <tburke3@sheffield.ac.uk>
2026-01-21 22:52:21 +0000

    fix pom.xml and empty test

---
+++ src/test/java/database/DatabaseConnectionHandlerTest.java
@@ -0,0 +1,9 @@
+package database;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseConnectionHandlerTest {
+
+}
